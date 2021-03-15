bbe9557afe48ba976a44400d1dfe873239edf2a4
harryfranklin <hrfranklin0702@gmail.com>
2021-03-15 17:26:38 +0000

    Update Touch_Detection_With_Bulb_V3.ino

--- Touch_Detection_With_Bulb_V3/Touch_Detection_With_Bulb_V3.ino
+++ Touch_Detection_With_Bulb_V3/Touch_Detection_With_Bulb_V3.ino
@@ -2,11 +2,11 @@
 // LED bulb turns off or off - dependent on Touchread being below a threshold
 // Pump in series with LED, LED on = Pump on
 
-# define led 32
+# define pump 32
 
 void setup() 
  {
-  pinMode(led, OUTPUT); 
+  pinMode(pump, OUTPUT); 
   Serial.begin(115200);
   delay(1000); 
   Serial.println("ESP Touch Test");
@@ -17,15 +17,15 @@ void loop()
   if ((touchRead(T5)) < 40) {
     Serial.print(touchRead(T5));
     Serial.println(" - Touch Detected"); // if T5 drops below 60, print "Touch Detected" and the value
-    delay(2500); // pause after detection so user can move hand away
-    digitalWrite(led, HIGH); // power LED and pump on GPIO 32
-    delay(1000); // power pump and LED for 1sec
+    delay(1500); // pause after detection so user can move hand away
+    digitalWrite(pump, HIGH); // power pump on GPIO 32
+    delay(1000); // power pump for 1sec
   }
                             
   if ((touchRead(T5)) >= 40) {
     Serial.println(touchRead(T5)); // if T5 is = or > 60, keep printing value of T5
     delay(1000);
-    digitalWrite(led, LOW); // do not power LED or pump on GPIO 32
+    digitalWrite(pump, LOW); // do not power pump on GPIO 32
   }
 
   delay(200);
